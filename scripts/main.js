ca50861005c20751f69b35d7da0c54ae4eb938cd
Max Orok <morok@mevex.com>
2019-01-07 14:41:52 -0500

    add shader compiler steps

--- scripts/main.js
+++ scripts/main.js
@@ -47,3 +47,46 @@ function main() {
         `;
 
 }
+
+// initialize a shader program
+
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // create the shader program
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // if we couldn't make the program, alert
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: '
+              + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+// create a shader, upload the source text and compile it
+// takes context, shader type and the source code string
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    // send source code to shader object
+    gl.shaderSource(shader, source);
+
+    // compile shader
+    gl.compileShader(shader);
+
+    // check that it compiled OK
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
